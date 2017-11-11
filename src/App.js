6ac0a8dc88e392c48de276cce19bdc5e6465818f
Anthony Sellitti <anthony.sellitti@envato.com>
2017-11-11 13:03:26 +1100

    Here's all the cards, how do they look?
    Fucking awesome. Yeah, thats right.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import logo from './logo.svg'
+import './App.css'
+import Card from "./components/Card.js"
 
 class App extends Component {
   render() {
@@ -10,9 +11,61 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div style={{display: "flex", flexWrap: "wrap"}}>
+          <Card size="30px" suit="diamonds" value="A"/>
+          <Card size="30px" suit="diamonds" value="2"/>
+          <Card size="30px" suit="diamonds" value="3"/>
+          <Card size="30px" suit="diamonds" value="4"/>
+          <Card size="30px" suit="diamonds" value="5"/>
+          <Card size="30px" suit="diamonds" value="6"/>
+          <Card size="30px" suit="diamonds" value="7"/>
+          <Card size="30px" suit="diamonds" value="8"/>
+          <Card size="30px" suit="diamonds" value="9"/>
+          <Card size="30px" suit="diamonds" value="10"/>
+          <Card size="30px" suit="diamonds" value="J"/>
+          <Card size="30px" suit="diamonds" value="Q"/>
+          <Card size="30px" suit="diamonds" value="K"/>
+          <Card size="30px" suit="hearts" value="A"/>
+          <Card size="30px" suit="hearts" value="2"/>
+          <Card size="30px" suit="hearts" value="3"/>
+          <Card size="30px" suit="hearts" value="4"/>
+          <Card size="30px" suit="hearts" value="5"/>
+          <Card size="30px" suit="hearts" value="6"/>
+          <Card size="30px" suit="hearts" value="7"/>
+          <Card size="30px" suit="hearts" value="8"/>
+          <Card size="30px" suit="hearts" value="9"/>
+          <Card size="30px" suit="hearts" value="10"/>
+          <Card size="30px" suit="hearts" value="J"/>
+          <Card size="30px" suit="hearts" value="Q"/>
+          <Card size="30px" suit="hearts" value="K"/>
+          <Card size="30px" suit="spades" value="A"/>
+          <Card size="30px" suit="spades" value="2"/>
+          <Card size="30px" suit="spades" value="3"/>
+          <Card size="30px" suit="spades" value="4"/>
+          <Card size="30px" suit="spades" value="5"/>
+          <Card size="30px" suit="spades" value="6"/>
+          <Card size="30px" suit="spades" value="7"/>
+          <Card size="30px" suit="spades" value="8"/>
+          <Card size="30px" suit="spades" value="9"/>
+          <Card size="30px" suit="spades" value="10"/>
+          <Card size="30px" suit="spades" value="J"/>
+          <Card size="30px" suit="spades" value="Q"/>
+          <Card size="30px" suit="spades" value="K"/>
+          <Card size="30px" suit="clubs" value="A"/>
+          <Card size="30px" suit="clubs" value="2"/>
+          <Card size="30px" suit="clubs" value="3"/>
+          <Card size="30px" suit="clubs" value="4"/>
+          <Card size="30px" suit="clubs" value="5"/>
+          <Card size="30px" suit="clubs" value="6"/>
+          <Card size="30px" suit="clubs" value="7"/>
+          <Card size="30px" suit="clubs" value="8"/>
+          <Card size="30px" suit="clubs" value="9"/>
+          <Card size="30px" suit="clubs" value="10"/>
+          <Card size="30px" suit="clubs" value="J"/>
+          <Card size="30px" suit="clubs" value="Q"/>
+          <Card size="30px" suit="clubs" value="K"/>
+
+        </div>
       </div>
     );
   }
