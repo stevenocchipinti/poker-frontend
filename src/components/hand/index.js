d57e6799ce6ad7952a8b321a10ad233ab3a9b6e7
Anthony Sellitti <anthony.sellitti@envato.com>
2017-11-12 15:42:03 +1100

    Pretend to fold by "throwing" cards up

--- src/components/hand/index.js
+++ src/components/hand/index.js
@@ -16,30 +16,51 @@ const offsetEm = 0.5;
 class Hand extends Component {
   constructor() {
     super()
-    this.state = { revealed: false }
+    this.state = {
+      revealed: false,
+      startY: 0,
+      endY: 0,
+      moving: false,
+    }
   }
 
-  handleTouchStart() {
-    this.setState({ revealed: true })
+  handleTouchStart(event) {
+    this.setState({
+      revealed: true,
+      moving: true,
+      startY: event.touches[0].clientY,
+      endY: event.touches[0].clientY,
+    })
   }
 
-  handleTouchEnd() {
-    this.setState({ revealed: false })
+  handleTouchEnd(event) {
+    this.setState({
+      revealed: false,
+      moving: false,
+      startY: 0,
+      endY: 0
+    })
+  }
+
+  handleTouchMove(event) {
+    this.setState({
+      endY: event.touches[0].clientY
+    })
   }
 
   render() {
-    const { revealed } = this.state;
+    const { revealed, startY, endY, moving } = this.state;
     const { children, visible } = this.props;
     const width = 3.55 + (children.length - 1) * offsetEm;
-    const transform = `translateY(${visible ? 0 : "-100vh"})`
+    const transform = `translateY(${visible ? `${Math.min(endY-startY,0)}px` : "-100vh"})`
+    const transition = !moving ? "400ms ease-in-out" : "initial"
 
     return (
       <div
-        style={{...styles.hand, width: `${width}em`, transform}}
-        onTouchStart={() => this.handleTouchStart()}
-        onTouchEnd={() => this.handleTouchEnd()}
-        onMouseDown={() => this.handleTouchStart()}
-        onMouseUp={() => this.handleTouchEnd()}
+        style={{...styles.hand, width: `${width}em`, transform, transition}}
+        onTouchStart={(event) => this.handleTouchStart(event)}
+        onTouchEnd={(event) => this.handleTouchEnd(event)}
+        onTouchMove={(event) => this.handleTouchMove(event)}
       >
         {
           Children.map(
@@ -54,6 +75,7 @@ class Hand extends Component {
             </div>
           ))
         }
+        <div>Much Lol: {startY} {endY}</div>
       </div>
     )
   }
